8732ea135a780b5310bc734c62ae3db4bc6d0049
MrHarsh002 <harshkumar26100@gmail.com>
2026-05-10 18:53:11 +0530

    change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api' || 'https://servicehub-pi.vercel.app/api';
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  (window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : 'https://servicehub-pi.vercel.app/api');
 
 const api = axios.create({
   baseURL: API_URL,
